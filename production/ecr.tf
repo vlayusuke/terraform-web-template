ee89473731b5870600786479d8c8fa6b9127c02d
vlayusuke <vlayusuke@gmail.com>
2026-04-26 04:14:29 +0900

    add: 本番環境のAmazon ECRの実装

---
+++ production/ecr.tf
@@ -0,0 +1,173 @@
+# ===============================================================================
+# Amazon ECR for NginX (Base Image)
+# ===============================================================================
+resource "aws_ecr_repository" "nginx_base" {
+  name                 = "${local.project}/${local.env}/base/nginx"
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name      = "${local.project}-${local.env}-ecr-nginx-base"
+    inspector = "true"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "nginx_base" {
+  repository = aws_ecr_repository.nginx_base.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
+
+
+# ===============================================================================
+# Amazon ECR for App (Base Image)
+# ===============================================================================
+resource "aws_ecr_repository" "app_base" {
+  name                 = "${local.project}/${local.env}/base/app"
+  image_tag_mutability = "IMMUTABLE"
+
+  tags = {
+    Name      = "${local.project}-${local.env}-ecr-app-base"
+    inspector = "true"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app_base" {
+  repository = aws_ecr_repository.app_base.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
+
+
+# ===============================================================================
+# Amazon ECR for NginX
+# ===============================================================================
+resource "aws_ecr_repository" "nginx" {
+  name                 = "${local.project}/${local.env}/nginx"
+  image_tag_mutability = "IMMUTABLE"
+
+  tags = {
+    Name      = "${local.project}-${local.env}-ecr-nginx"
+    inspector = "true"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  repository = aws_ecr_repository.nginx.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
+
+
+# ===============================================================================
+# Amazon ECR for App
+# ===============================================================================
+resource "aws_ecr_repository" "app" {
+  name                 = "${local.project}/${local.env}/app"
+  image_tag_mutability = "IMMUTABLE"
+
+  tags = {
+    Name      = "${local.project}-${local.env}-ecr-app"
+    inspector = "true"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
+
+
+# ===============================================================================
+# Amazon ECR for Fluent Bit
+# ===============================================================================
+resource "aws_ecr_repository" "fluent_bit" {
+  name                 = "${local.project}/${local.env}/fluent-bit"
+  image_tag_mutability = "IMMUTABLE"
+
+  tags = {
+    Name      = "${local.project}-${local.env}-ecr-fluent-bit"
+    inspector = "true"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "fluent_bit" {
+  repository = aws_ecr_repository.fluent_bit.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Keep last 10 images",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+}
