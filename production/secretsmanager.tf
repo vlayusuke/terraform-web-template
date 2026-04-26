c1106565c020ff19161c8bffd5f0a25012af751c
vlayusuke <vlayusuke@gmail.com>
2026-04-27 00:34:14 +0900

    add: 本番環境のAWS Secrets Managerの実装

---
+++ production/secretsmanager.tf
@@ -0,0 +1,20 @@
+# ===============================================================================
+# AWS Secrets Manager for Docker Hub Credentials
+# ===============================================================================
+resource "aws_secretsmanager_secret" "dockerhub" {
+  name                    = "${local.project}-${local.env}-smg-dockerhub-credentials"
+  kms_key_id              = aws_kms_key.application.arn
+  recovery_window_in_days = 7
+
+  tags = {
+    Name = "${local.project}-${local.env}-smg-dockerhub-credentials"
+  }
+}
+
+resource "aws_secretsmanager_secret_version" "dockerhub" {
+  secret_id = aws_secretsmanager_secret.dockerhub.id
+  secret_string = jsonencode({
+    username = var.dockerhub_username
+    password = var.dockerhub_password
+  })
+}
