03a2e90d802b320505e24e87c074f6920ec23f85
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-01 03:31:17 +0900

    add: Amazon Inspectorの実装

---
+++ audit/inspector.tf
@@ -0,0 +1,14 @@
+# ===============================================================================
+# Amazon Inspector v2
+# ===============================================================================
+resource "aws_inspector2_enabler" "default" {
+  account_ids = [
+    data.aws_caller_identity.current.account_id,
+  ]
+  resource_types = [
+    "EC2",
+    "ECR",
+    "LAMBDA",
+    "LAMBDA_CODE",
+  ]
+}
