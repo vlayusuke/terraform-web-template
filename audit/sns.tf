62c66963e33b8768a89059c033b4dddd5a5974f4
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-02 12:39:33 +0900

    add: auditのAmazon SNSの実装

---
+++ audit/sns.tf
@@ -0,0 +1,118 @@
+# ===============================================================================
+# Amazon SNS Topic for AuditNotification to Slack
+# ===============================================================================
+resource "aws_sns_topic" "to_slack_audit" {
+  name = "${local.project}-${local.env}-sns-to-slack-audit"
+
+  delivery_policy = jsonencode({
+    "http" : {
+      "defaultHealthyRetryPolicy" : {
+        "minDelayTarget" : 20,
+        "maxDelayTarget" : 20,
+        "numRetries" : 3,
+        "numMaxDelayRetries" : 0,
+        "numNoDelayRetries" : 0,
+        "numMinDelayRetries" : 0,
+        "backoffFunction" : "linear"
+      },
+      "disableSubscriptionOverrides" : false
+    }
+  })
+
+  tags = {
+    Name = "${local.project}-${local.env}-sns-to-slack-audit"
+  }
+}
+
+resource "aws_sns_topic_policy" "to_slack_audit" {
+  arn = aws_sns_topic.to_slack_audit.arn
+
+  policy = data.aws_iam_policy_document.to_slack_audit.json
+}
+
+data "aws_iam_policy_document" "to_slack_audit" {
+  statement {
+    sid    = "SNSAccess"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+      "sns:RemovePermission",
+      "sns:SetTopicAttributes",
+      "sns:DeleteTopic",
+      "sns:ListSubscriptionsByTopic",
+      "sns:GetTopicAttributes",
+      "sns:AddPermission",
+      "sns:Subscribe",
+    ]
+    resources = [
+      aws_sns_topic.to_slack_audit.arn,
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values = [
+        data.aws_caller_identity.current.account_id,
+      ]
+    }
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "SNSPublish"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [
+      aws_sns_topic.to_slack_audit.arn,
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+      ]
+    }
+  }
+}
+
+
+# ===============================================================================
+# Amazon SNS Topic for AWS Config Notification to Slack
+# ===============================================================================
+resource "aws_sns_topic" "config_notifications" {
+  name = "${local.project}-${local.env}-sns-config-notifications"
+
+  tags = {
+    Name = "${local.project}-${local.env}-sns-config-notifications"
+  }
+}
+
+resource "aws_sns_topic_policy" "config_notifications_policy" {
+  arn    = aws_sns_topic.config_notifications.arn
+  policy = data.aws_iam_policy_document.config_notifications_policy.json
+}
+
+data "aws_iam_policy_document" "config_notifications_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+      ]
+    }
+    resources = [
+      aws_sns_topic.config_notifications.arn,
+    ]
+  }
+}
