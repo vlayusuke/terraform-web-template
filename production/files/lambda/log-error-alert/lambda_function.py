cd2e21e38325bf9335d90b72f8cd8eaac2aae602
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-13 02:30:27 +0900

    fix: 本番環境のLambda関数のログ出力方式を修正

--- production/files/lambda/log-error-alert/lambda_function.py
+++ production/files/lambda/log-error-alert/lambda_function.py
@@ -16,13 +16,15 @@ headers = {
 
 
 def lambda_handler(event, context):
-    print(event)
+    logger.info(event)
+
     data = event['awslogs']['data']
     json_str = zlib.decompress(base64.b64decode(data), 16 + zlib.MAX_WBITS).decode('utf-8')
     json_data = json.loads(json_str)
 
     for row in json_data['logEvents']:
-        print(row)
+        logger.info(row)
+
         log_message = '''
             Log group : `{logGroup}`\nLog stream : {logStream}\n<https://ap-northeast-1.console.aws.amazon.com/cloudwatch/home?region=ap-northeast-1#logEventViewer:group={logGroup};stream={logStream} | 詳細はここをクリック>
             ```{message}```
@@ -40,4 +42,4 @@ def lambda_handler(event, context):
 
         with request.urlopen(req) as res:
             content = res.read()
-            print(content)
+            logger.info(content)
