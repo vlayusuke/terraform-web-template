7d0c5934cf2b6ea28a759cd44e3e44860fe590fa
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-16 07:00:07 +0900

    fix: 開発環境のAmazon ElastiCacheの設定変更

--- develop/elasticache.tf
+++ develop/elasticache.tf
@@ -3,9 +3,9 @@
 # ===============================================================================
 resource "aws_elasticache_replication_group" "redis" {
   replication_group_id       = "${local.project}-${local.env}-ec-redis-cluster"
-  description                = "Amazon ElastiCache Replication group for ${local.project}"
+  description                = "Amazon ElastiCache Replication group for ${local.project}-${local.env}"
   engine                     = "redis"
-  engine_version             = "7.1"
+  engine_version             = local.elasticache_redis_version
   node_type                  = "cache.t4g.medium"
   num_cache_clusters         = 2
   multi_az_enabled           = true
@@ -13,7 +13,7 @@ resource "aws_elasticache_replication_group" "redis" {
   port                       = 6379
   parameter_group_name       = aws_elasticache_parameter_group.redis.id
   subnet_group_name          = aws_elasticache_subnet_group.redis.name
-  maintenance_window         = "sat:14:00-sat:15:00"
+  maintenance_window         = "sat:15:00-sat:16:00"
   snapshot_retention_limit   = 14
   snapshot_window            = "15:00-16:00"
   notification_topic_arn     = aws_sns_topic.event_notification.arn
@@ -29,7 +29,7 @@ resource "aws_elasticache_replication_group" "redis" {
 
 resource "aws_elasticache_subnet_group" "redis" {
   name        = "${local.project}-${local.env}-ec-redis-cluster-subg"
-  description = "Subnet group for ${local.project} Amazon ElastiCache Cluster"
+  description = "Subnet group for ${local.project}-${local.env} Amazon ElastiCache Cluster"
   subnet_ids = [
     for subnet in aws_subnet.main_private :
     subnet.id
@@ -46,7 +46,7 @@ resource "aws_elasticache_subnet_group" "redis" {
 # ===============================================================================
 resource "aws_elasticache_parameter_group" "redis" {
   name        = "${local.project}-${local.env}-ec-redis-cache-params-ecpg"
-  description = "Parameter group for ${local.project} Amazon ElastiCache Cluster"
+  description = "Parameter group for ${local.project}-${local.env} Amazon ElastiCache Cluster"
   family      = "redis7"
 
   parameter {
