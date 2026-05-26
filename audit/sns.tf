d4cf81ffe2e7ff0100aff72b6cda5c1bfa757f86
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-27 03:51:33 +0900

    fix: auditのAmazon SNSのリソース追加と修正

--- audit/sns.tf
+++ audit/sns.tf
@@ -1,5 +1,5 @@
 # ===============================================================================
-# Amazon SNS Topic for Audit Event Notification
+# Amazon SNS Topic for Audit Event Notification (ap-northeast-1)
 # ===============================================================================
 resource "aws_sns_topic" "event_notifications_audit" {
   name = "${local.project}-${local.env}-sns-event-notifications-audit"
@@ -25,8 +25,7 @@ resource "aws_sns_topic" "event_notifications_audit" {
 }
 
 resource "aws_sns_topic_policy" "event_notifications_audit" {
-  arn = aws_sns_topic.event_notifications_audit.arn
-
+  arn    = aws_sns_topic.event_notifications_audit.arn
   policy = data.aws_iam_policy_document.event_notifications_audit.json
 }
 
@@ -72,6 +71,92 @@ data "aws_iam_policy_document" "event_notifications_audit" {
     ]
     resources = [
       aws_sns_topic.event_notifications_audit.arn,
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "events.amazonaws.com",
+      ]
+    }
+  }
+}
+
+
+# ===============================================================================
+# Amazon SNS Topic for Audit Event Notification (us-east-1)
+# ===============================================================================
+resource "aws_sns_topic" "event_notifications_audit_global" {
+  provider = aws.virginia
+  name     = "${local.project}-${local.env}-sns-event-notifications-audit-global"
+
+  delivery_policy = jsonencode({
+    "http" : {
+      "defaultHealthyRetryPolicy" : {
+        "minDelayTarget" : 20,
+        "maxDelayTarget" : 20,
+        "numRetries" : 3,
+        "numMaxDelayRetries" : 0,
+        "numNoDelayRetries" : 0,
+        "numMinDelayRetries" : 0,
+        "backoffFunction" : "linear"
+      },
+      "disableSubscriptionOverrides" : false
+    }
+  })
+
+  tags = {
+    Name = "${local.project}-${local.env}-sns-event-notifications-audit-global"
+  }
+}
+
+resource "aws_sns_topic_policy" "event_notifications_audit_global" {
+  provider = aws.virginia
+  arn      = aws_sns_topic.event_notifications_audit_global.arn
+  policy   = data.aws_iam_policy_document.event_notifications_audit_global.json
+}
+
+data "aws_iam_policy_document" "event_notifications_audit_global" {
+  statement {
+    sid    = "SNSAccess"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+      "sns:RemovePermission",
+      "sns:SetTopicAttributes",
+      "sns:DeleteTopic",
+      "sns:ListSubscriptionsByTopic",
+      "sns:GetTopicAttributes",
+      "sns:AddPermission",
+      "sns:Subscribe",
+    ]
+    resources = [
+      aws_sns_topic.event_notifications_audit_global.arn,
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+      values = [
+        data.aws_caller_identity.current.account_id,
+      ]
+    }
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*",
+      ]
+    }
+  }
+
+  statement {
+    sid    = "SNSPublish"
+    effect = "Allow"
+    actions = [
+      "sns:Publish",
+    ]
+    resources = [
+      aws_sns_topic.event_notifications_audit_global.arn,
     ]
     principals {
       type = "Service"
