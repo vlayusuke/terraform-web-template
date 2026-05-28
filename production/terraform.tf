a17101accb0e26500c30afb88d219d1da4702ea1
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-05-28 09:51:25 +0900

    fix: 各ProviderのUpgrade

--- production/terraform.tf
+++ production/terraform.tf
@@ -14,12 +14,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.46.0"
+      version = "6.47.0"
     }
 
     awscc = {
       source  = "hashicorp/awscc"
-      version = "1.85.0"
+      version = "1.86.0"
     }
   }
 }
