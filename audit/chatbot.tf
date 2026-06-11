70045ea44a8e3b7c23b618910cef1e08994ef582
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-06-11 09:55:31 +0900

    add: audit向けのTeams用AWS Chatbotの設定をコメントで追加

--- audit/chatbot.tf
+++ audit/chatbot.tf
@@ -22,3 +22,26 @@ resource "aws_chatbot_slack_channel_configuration" "chatbot_audit_notifications_
     Name = "${local.project}-${local.env}-chatbot-notifications-for-slack"
   }
 }
+
+# If you want to use Microsoft Teams, you can use the following resource instead of the above one.
+# resource "aws_chatbot_microsoft_teams_channel_configuration" "chatbot_audit_notifications_for_teams" {
+#   configuration_name          = "${local.project}-${local.env}-chatbot-notifications-for-teams"
+#   iam_role_arn                = aws_iam_role.chatbot_audit.arn
+#   microsoft_teams_channel_id   = var.teams_channel_id
+#   microsoft_teams_tenant_id    = var.teams_tenant_id
+#   logging_level               = "INFO"
+#   user_authorization_required = false
+#
+#   guardrail_policy_arns = [
+#     aws_iam_policy.chatbot_audit_guardrail.arn,
+#   ]
+#
+#   sns_topic_arns = [
+#     aws_sns_topic.config_notifications.arn,
+#     aws_sns_topic.event_notifications_audit.arn,
+#   ]
+#
+#   tags = {
+#     Name = "${local.project}-${local.env}-chatbot-notifications-for-teams"
+#   }
+# }
