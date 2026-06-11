cf9c39172692115cc8a368f38804792801e46b13
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-06-11 13:12:44 +0900

    fix: 各環境のAWS Chatbotのリソース名を修正

--- develop/chatbot.tf
+++ develop/chatbot.tf
@@ -1,8 +1,8 @@
 # ===============================================================================
 # AWS Chatbot (Amazon Q Developer)
 # ===============================================================================
-resource "aws_chatbot_slack_channel_configuration" "chatbot_notification_for_slack" {
-  configuration_name          = "${local.project}-${local.env}-chatbot-notification-for-slack"
+resource "aws_chatbot_slack_channel_configuration" "chatbot_notifications_for_slack" {
+  configuration_name          = "${local.project}-${local.env}-chatbot-notifications-for-slack"
   iam_role_arn                = aws_iam_role.chatbot.arn
   slack_channel_id            = var.slack_channel_id
   slack_team_id               = var.slack_workspace_id
@@ -16,21 +16,21 @@ resource "aws_chatbot_slack_channel_configuration" "chatbot_notification_for_sla
   sns_topic_arns = [
     aws_sns_topic.metric_alarm.arn,
     aws_sns_topic.event_alarm.arn,
-    aws_sns_topic.inspector_notification.arn,
-    aws_sns_topic.event_notification.arn,
+    aws_sns_topic.inspector_notifications.arn,
+    aws_sns_topic.event_notifications.arn,
   ]
 
   tags = {
-    Name = "${local.project}-${local.env}-chatbot-notification-for-slack"
+    Name = "${local.project}-${local.env}-chatbot-notifications-for-slack"
   }
 }
 
 # If you want to use Microsoft Teams, you can use the following resource instead of the above one.
-# resource "aws_chatbot_microsoft_teams_channel_configuration" "chatbot_notification_for_teams" {
-#   configuration_name          = "${local.project}-${local.env}-chatbot-notification-for-teams"
+# resource "aws_chatbot_microsoft_teams_channel_configuration" "chatbot_notifications_for_teams" {
+#   configuration_name          = "${local.project}-${local.env}-chatbot-notifications-for-teams"
 #   iam_role_arn                = aws_iam_role.chatbot.arn
-#   microsoft_teams_channel_id   = var.teams_channel_id
-#   microsoft_teams_tenant_id    = var.teams_tenant_id
+#   microsoft_teams_channel_id  = var.teams_channel_id
+#   microsoft_teams_tenant_id   = var.teams_tenant_id
 #   logging_level               = "INFO"
 #   user_authorization_required = false
 #
@@ -41,11 +41,11 @@ resource "aws_chatbot_slack_channel_configuration" "chatbot_notification_for_sla
 #   sns_topic_arns = [
 #     aws_sns_topic.metric_alarm.arn,
 #     aws_sns_topic.event_alarm.arn,
-#     aws_sns_topic.inspector_notification.arn,
-#     aws_sns_topic.event_notification.arn,
+#     aws_sns_topic.inspector_notifications.arn,
+#     aws_sns_topic.event_notifications.arn,
 #   ]
 #
 #   tags = {
-#     Name = "${local.project}-${local.env}-chatbot-notification-for-teams"
+#     Name = "${local.project}-${local.env}-chatbot-notifications-for-teams"
 #   }
 # }
