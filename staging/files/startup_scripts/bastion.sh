7ef4da39dfc8d016f522ff9254fc55bc21fc4039
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-06-16 03:21:35 +0900

    add: ステージング環境踏み台サーバー用EC2インスタンスへのスクリプトを追加

--- staging/files/startup_scripts/bastion.sh
+++ staging/files/startup_scripts/bastion.sh
@@ -3,8 +3,14 @@
 # This script is used to configure the bastion host for the project.
 amazon-linux-extras install -y epel
 yum update -y
-yum install -y epel-release jq git vim mysql python3 amazon-ssm-agent
+yum install -y epel-release jq git vim mysql python3 amazon-ssm-agent awslogs
+dnf upgrade aws-cli2 --releasever=latest
 systemctl enable amazon-ssm-agent
+
+sed -i -e 's/us-east-1/ap-northeast-1/g' /etc/awslogs/awscli.conf
+aws s3 cp s3://${bastion_bucket}/awslogs.conf /etc/awslogs/awslogs.conf
+chmod 644 /etc/awslogs/awslogs.conf
+systemctl enable awslogsd.service
 
 echo "Defaults logfile=/var/log/sudo.log" >> /etc/sudoers
 echo "PROMPT_COMMAND=\"$PROMPT_COMMAND; history -a\"" >> /root/.bashprofile
