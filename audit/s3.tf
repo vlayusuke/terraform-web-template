9344e87dd56b069f64f31bf6113c96de3cd06f01
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-06-21 05:31:10 +0900

    fix: auditのAmazon S3をap-northeast-1のAWSリソースを対象に構成するよう修正

--- audit/s3.tf
+++ audit/s3.tf
@@ -1,5 +1,5 @@
 # ===============================================================================
-# Amazon S3 Bucket for AWS CloudTrail (ap-northeast-1)
+# Amazon S3 Bucket for AWS CloudTrail
 # ===============================================================================
 resource "aws_s3_bucket" "cloudtrail_logs" {
   bucket = "${local.project}-${local.env}-${local.account_id}-s3-ctl-logs-bucket"
@@ -102,6 +102,7 @@ data "aws_iam_policy_document" "cloudtrail_logs" {
     actions = [
       "s3:GetBucketAcl",
       "s3:ListBucket",
+      "s3:GetBucketLocation",
     ]
     resources = [
       aws_s3_bucket.cloudtrail_logs.arn,
@@ -120,6 +121,7 @@ data "aws_iam_policy_document" "cloudtrail_logs" {
     effect = "Allow"
     actions = [
       "s3:PutObject",
+      "s3:PutObjectAcl",
     ]
     resources = [
       "${aws_s3_bucket.cloudtrail_logs.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
@@ -170,367 +172,7 @@ data "aws_iam_policy_document" "cloudtrail_logs" {
 
 
 # ===============================================================================
-# Amazon S3 Bucket for AWS CloudTrail (ap-northeast-3)
-# ===============================================================================
-resource "aws_s3_bucket" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = "${local.project}-${local.env}-${local.account_id}-s3-ctl-logs-osaka-bucket"
-
-  tags = {
-    Name = "${local.project}-${local.env}-${local.account_id}-s3-ctl-logs-osaka-bucket"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-  acl      = "log-delivery-write"
-
-  depends_on = [
-    aws_s3_bucket_ownership_controls.cloudtrail_logs_osaka,
-  ]
-}
-
-resource "aws_s3_bucket_public_access_block" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.bucket
-
-  rule {
-    blocked_encryption_types = [
-      "SSE-C"
-    ]
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-    bucket_key_enabled = false
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-
-  rule {
-    id     = "delete-object"
-    status = "Enabled"
-
-    filter {
-      object_size_greater_than = 0
-    }
-
-    expiration {
-      days = local.expire_days
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = local.expire_days
-    }
-  }
-
-  depends_on = [
-    aws_s3_bucket_versioning.cloudtrail_logs_osaka,
-  ]
-}
-
-resource "aws_s3_bucket_versioning" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_logging" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-
-  target_bucket = aws_s3_bucket.s3_logs.id
-  target_prefix = "cloudtrail-logs-osaka/"
-
-}
-
-resource "aws_s3_bucket_policy" "cloudtrail_logs_osaka" {
-  provider = aws.osaka
-  bucket   = aws_s3_bucket.cloudtrail_logs_osaka.id
-  policy   = data.aws_iam_policy_document.cloudtrail_logs_osaka.json
-}
-
-data "aws_iam_policy_document" "cloudtrail_logs_osaka" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
-    actions = [
-      "s3:GetBucketAcl",
-      "s3:ListBucket",
-    ]
-    resources = [
-      aws_s3_bucket.cloudtrail_logs_osaka.arn,
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "cloudtrail.amazonaws.com",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-    ]
-    resources = [
-      "${aws_s3_bucket.cloudtrail_logs_osaka.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "cloudtrail.amazonaws.com",
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values = [
-        "bucket-owner-full-control",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "EnforceSSL"
-    effect = "Deny"
-    actions = [
-      "s3:*",
-    ]
-    resources = [
-      aws_s3_bucket.cloudtrail_logs_osaka.arn,
-      "${aws_s3_bucket.cloudtrail_logs_osaka.arn}/*",
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values = [
-        "false",
-      ]
-    }
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "*",
-      ]
-    }
-  }
-}
-
-
-# ===============================================================================
-# Amazon S3 Bucket for AWS CloudTrail (us-east-1)
-# ===============================================================================
-resource "aws_s3_bucket" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = "${local.project}-${local.env}-${local.account_id}-s3-ctl-logs-global-bucket"
-
-  tags = {
-    Name = "${local.project}-${local.env}-${local.account_id}-s3-ctl-logs-global-bucket"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-  acl      = "log-delivery-write"
-
-  depends_on = [
-    aws_s3_bucket_ownership_controls.cloudtrail_logs_global,
-  ]
-}
-
-resource "aws_s3_bucket_public_access_block" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.bucket
-
-  rule {
-    blocked_encryption_types = [
-      "SSE-C"
-    ]
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-    bucket_key_enabled = false
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-
-  rule {
-    id     = "delete-object"
-    status = "Enabled"
-
-    filter {
-      object_size_greater_than = 0
-    }
-
-    expiration {
-      days = local.expire_days
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = local.expire_days
-    }
-  }
-
-  depends_on = [
-    aws_s3_bucket_versioning.cloudtrail_logs_global,
-  ]
-}
-
-resource "aws_s3_bucket_versioning" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_logging" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-
-  target_bucket = aws_s3_bucket.s3_logs.id
-  target_prefix = "cloudtrail-logs-global/"
-
-}
-
-resource "aws_s3_bucket_policy" "cloudtrail_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.cloudtrail_logs_global.id
-  policy   = data.aws_iam_policy_document.cloudtrail_logs_global.json
-}
-
-data "aws_iam_policy_document" "cloudtrail_logs_global" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
-    actions = [
-      "s3:GetBucketAcl",
-      "s3:ListBucket",
-    ]
-    resources = [
-      aws_s3_bucket.cloudtrail_logs_global.arn,
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "cloudtrail.amazonaws.com",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-    ]
-    resources = [
-      "${aws_s3_bucket.cloudtrail_logs_global.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "cloudtrail.amazonaws.com",
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values = [
-        "bucket-owner-full-control",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "EnforceSSL"
-    effect = "Deny"
-    actions = [
-      "s3:*",
-    ]
-    resources = [
-      aws_s3_bucket.cloudtrail_logs_global.arn,
-      "${aws_s3_bucket.cloudtrail_logs_global.arn}/*",
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values = [
-        "false",
-      ]
-    }
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "*",
-      ]
-    }
-  }
-}
-
-
-# ===============================================================================
-# Amazon S3 Bucket for AWS Config (ap-northeast-1)
+# Amazon S3 Bucket for AWS Config
 # ===============================================================================
 resource "aws_s3_bucket" "config_logs" {
   bucket = "${local.project}-${local.env}-${local.account_id}-s3-cfg-logs-bucket"
@@ -710,195 +352,6 @@ data "aws_iam_policy_document" "config_logs" {
 
 
 # ===============================================================================
-# Amazon S3 Bucket for AWS Config (us-east-1)
-# ===============================================================================
-resource "aws_s3_bucket" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = "${local.project}-${local.env}-${local.account_id}-s3-cfg-logs-global-bucket"
-
-  tags = {
-    Name = "${local.project}-${local.env}-${local.account_id}-s3-cfg-logs-global-bucket"
-  }
-}
-
-resource "aws_s3_bucket_ownership_controls" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-  acl      = "log-delivery-write"
-
-  depends_on = [
-    aws_s3_bucket_ownership_controls.config_logs_global,
-  ]
-}
-
-resource "aws_s3_bucket_public_access_block" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.bucket
-
-  rule {
-    blocked_encryption_types = [
-      "SSE-C"
-    ]
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-    bucket_key_enabled = false
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-
-  rule {
-    id     = "delete-object"
-    status = "Enabled"
-
-    filter {
-      object_size_greater_than = 0
-    }
-
-    expiration {
-      days = local.expire_days
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = local.expire_days
-    }
-  }
-
-  depends_on = [
-    aws_s3_bucket_versioning.config_logs_global,
-  ]
-}
-
-resource "aws_s3_bucket_versioning" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_logging" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-
-  target_bucket = aws_s3_bucket.s3_logs.id
-  target_prefix = "config-logs-global/"
-
-}
-
-resource "aws_s3_bucket_policy" "config_logs_global" {
-  provider = aws.virginia
-  bucket   = aws_s3_bucket.config_logs_global.id
-  policy   = data.aws_iam_policy_document.config_logs_global.json
-}
-
-data "aws_iam_policy_document" "config_logs_global" {
-  statement {
-    sid    = "EnforceSSL"
-    effect = "Deny"
-    actions = [
-      "s3:*",
-    ]
-    resources = [
-      aws_s3_bucket.config_logs_global.arn,
-      "${aws_s3_bucket.config_logs_global.arn}/*",
-    ]
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values = [
-        "false",
-      ]
-    }
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "*",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AWSConfigBucketPermissionsCheck"
-    effect = "Allow"
-    actions = [
-      "s3:GetBucketAcl",
-    ]
-    resources = [
-      aws_s3_bucket.config_logs_global.arn,
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "config.amazonaws.com",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AWSConfigBucketExistenceCheck"
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket",
-    ]
-    resources = [
-      aws_s3_bucket.config_logs_global.arn,
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "config.amazonaws.com",
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AWSConfigBucketDelivery"
-    effect = "Allow"
-    actions = [
-      "s3:PutObject",
-    ]
-    resources = [
-      "${aws_s3_bucket.config_logs_global.arn}/*",
-    ]
-
-    principals {
-      type = "Service"
-      identifiers = [
-        "config.amazonaws.com",
-      ]
-    }
-  }
-}
-
-
-# ===============================================================================
 # Amazon S3 Bucket for Amazon GuardDuty Logs
 # ===============================================================================
 resource "aws_s3_bucket" "guardduty_logs" {
@@ -1023,10 +476,11 @@ data "aws_iam_policy_document" "guardduty_logs" {
   }
 
   statement {
-    sid    = "AWSConfigBucketPermissionsCheck"
+    sid    = "GuardDutyBucketPermissionsCheck"
     effect = "Allow"
     actions = [
       "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
     ]
     resources = [
       aws_s3_bucket.guardduty_logs.arn,
@@ -1041,7 +495,7 @@ data "aws_iam_policy_document" "guardduty_logs" {
   }
 
   statement {
-    sid    = "AWSConfigBucketExistenceCheck"
+    sid    = "GuardDutyBucketExistenceCheck"
     effect = "Allow"
     actions = [
       "s3:ListBucket",
@@ -1059,10 +513,11 @@ data "aws_iam_policy_document" "guardduty_logs" {
   }
 
   statement {
-    sid    = "AWSConfigBucketDelivery"
+    sid    = "GuardDutyBucketDelivery"
     effect = "Allow"
     actions = [
       "s3:PutObject",
+      "s3:PutObjectAcl",
     ]
     resources = [
       "${aws_s3_bucket.guardduty_logs.arn}/*",
