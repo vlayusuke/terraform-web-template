164ea1820b788a6752dba91c6f5bb3f04011f908
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-07-04 01:35:05 +0900

    fix: ステージング環境Amazon ElastiCacheのメンテナンス時刻を調整

--- staging/elasticache.tf
+++ staging/elasticache.tf
@@ -13,9 +13,9 @@ resource "aws_elasticache_replication_group" "redis" {
   port                       = 6379
   parameter_group_name       = aws_elasticache_parameter_group.redis.id
   subnet_group_name          = aws_elasticache_subnet_group.redis.name
-  maintenance_window         = "sat:16:10-sat:17:10"
+  maintenance_window         = "sat:21:30-sat:22:30"
   snapshot_retention_limit   = 14
-  snapshot_window            = "15:00-16:00"
+  snapshot_window            = "21:00-22:00"
   notification_topic_arn     = aws_sns_topic.event_notifications.arn
   auto_minor_version_upgrade = true
   apply_immediately          = true
