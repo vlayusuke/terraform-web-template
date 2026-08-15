3d72cea7f10d97b8650b762db520b14c986173f8
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-08-16 04:47:11 +0900

    add: 開発環境のAWS Lambda関数の設定を追加

--- develop/lambda.tf
+++ develop/lambda.tf
@@ -31,6 +31,7 @@ resource "aws_lambda_function" "lambda_log_error_alert" {
 
   depends_on = [
     aws_kms_key.lambda,
+    aws_iam_role.lambda_cloudwatch,
   ]
 
   tags = {
@@ -86,6 +87,7 @@ resource "aws_lambda_function" "lambda_metric_alarm" {
   }
 
   depends_on = [
+    aws_iam_role.lambda_cloudwatch,
     aws_kms_key.lambda,
   ]
 
@@ -135,6 +137,7 @@ resource "aws_lambda_function" "lambda_rds_control" {
   }
 
   depends_on = [
+    aws_iam_role.lambda_rds_control,
     aws_kms_key.lambda,
   ]
 
@@ -163,7 +166,7 @@ resource "aws_lambda_function_event_invoke_config" "lambda_rds_control" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.lambda_rds_control,
+    aws_iam_role.lambda_rds_control,
   ]
 }
 
@@ -210,6 +213,7 @@ resource "aws_lambda_function" "lambda_schedule_ecs_maintenance" {
   }
 
   depends_on = [
+    aws_iam_role.lambda_schedule_ecs_maintenance,
     aws_kms_key.lambda,
   ]
 
@@ -232,7 +236,7 @@ resource "aws_lambda_function_event_invoke_config" "lambda_schedule_ecs_maintena
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.lambda_schedule_ecs_maintenance,
+    aws_iam_role.lambda_schedule_ecs_maintenance,
   ]
 }
 
@@ -285,6 +289,7 @@ resource "aws_lambda_function" "lambda_execute_ecs_force_deployment" {
   }
 
   depends_on = [
+    aws_iam_role.lambda_execute_ecs_force_deployment,
     aws_kms_key.lambda,
   ]
 
@@ -313,7 +318,7 @@ resource "aws_lambda_function_event_invoke_config" "lambda_execute_ecs_force_dep
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.lambda_execute_ecs_force_deployment,
+    aws_iam_role.lambda_execute_ecs_force_deployment,
   ]
 }
 
