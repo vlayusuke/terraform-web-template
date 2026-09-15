e09b307d5df6919577d8443d2dc0bfe08761a9d3
Yusuke TOMIOKA <vlayusuke@gmail.com>
2026-09-16 03:49:18 +0900

    add: AWS CodeCommitの承認ルールを作成

--- codecommit.tf
+++ codecommit.tf
@@ -11,3 +11,31 @@ resource "aws_codecommit_repository" "codecommit" {
     Name = "${local.project}-${local.env}-cmt-${trimprefix(local.repository, "${local.repository_name}/")}"
   }
 }
+
+
+# ================================================================================
+# AWS CodeCommit Approval Rule
+# ================================================================================
+resource "aws_codecommit_approval_rule_template" "codecommit_approval_rule" {
+  name        = "${local.project}-${local.env}-cmt-approval-rule"
+  description = "Approval rule template for ${local.repository} project."
+
+  content = jsonencode({
+    Version               = "2018-11-08"
+    DestinationReferences = ["refs/heads/main"]
+    Statement = [
+      {
+        Type                    = "Approvers"
+        NumberOfApprovalsNeeded = 1
+        ApprovalPoolMembers = [
+          "arn:aws:sts::${data.aws_caller_identity.current.account_id}:assumed-role/CodeCommitReviewersRole/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_codecommit_approval_rule_template_association" "codecommit_approval_rule_association" {
+  approval_rule_template_name = aws_codecommit_approval_rule_template.codecommit_approval_rule.name
+  repository_name             = aws_codecommit_repository.codecommit.repository_name
+}
